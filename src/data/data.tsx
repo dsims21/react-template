ddc876f247681b48c6fc5a0f1065273a67b3d2e1
Daniel Sims <dsims21@gmail.com>
2023-07-11 18:54:39 -0700

    p p

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -315,9 +315,15 @@ export const experience: TimelineItem[] = [
     location: 'Starburst Data',	
     title: 'Manager, Technical Account Management',	
     content: (
+      <div>
       <p>
         I managed a specialized team of technical account managers who catered to high-profile clients, navigated complex challenges, and implemented systems to enhance operational efficiency. My focus was on using innovative solutions and clear communication to achieve customer satisfaction and drive team performance.
       </p>
+      <br />
+      <p>
+        • testimonial
+      </p>
+      </div>
     ),
   },
   {
