3e7b185e1d1a6b4d3b781d2983c7e141a1c1d0f5
Daniel Sims <dsims21@gmail.com>
2023-07-13 12:32:34 -0700

    luke before lexi fix?

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -487,14 +487,14 @@ export const testimonial: TestimonialSection = {
       image: 'testimonialimages/kevin.jpg',
     },
     {
-      name: 'Lexi Wilkins (Project Manager)',
-      text: `Daniel possess exceptional problem-solving skills and can approach complex situations with a calm and analytical mindset.`,
-      image: 'testimonialimages/lexi.jpg',
-    },
-    {
       name: 'Luke Watkins (Senior Technical Account Manager)',
       text: `What makes Daniel standout is his data driven and "back to fundamentals" style of working, and is a leader that inspires through action. He is highly technical leader in the data/AI/ML domain paired with a strong business & people mindset.`,
       image: 'testimonialimages/luke.jpg',
+    },
+    {
+      name: 'Lexi Wilkins (Project Manager)',
+      text: `Daniel possess exceptional problem-solving skills and can approach complex situations with a calm and analytical mindset.`,
+      image: 'testimonialimages/lexi.jpg',
     },
     {
       name: 'JB Oh (Technical Account Manager)',
