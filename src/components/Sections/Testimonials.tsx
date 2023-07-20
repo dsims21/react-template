0c6782052c62f6bbd985091548b19c2955673ebd
Daniel Sims <dsims21@gmail.com>
2023-07-19 18:33:52 -0700

    use interval 30s

--- src/components/Sections/Testimonials.tsx
+++ src/components/Sections/Testimonials.tsx
@@ -62,7 +62,7 @@ const Testimonials: FC = memo(() => {
     setScrollValue(event.currentTarget.scrollLeft);
   }, []);
 
-  useInterval(next, 15000);
+  useInterval(next, 30000);
 
   // If no testimonials, don't render the section
   if (!testimonials.length) {
